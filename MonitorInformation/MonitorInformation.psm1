557d5a111cd67942c3835d6a70a299f1b0abf70d
Robin Stolpe <robin@stolpe.io>
2023-01-08 11:42:29 +0100

    updaet

--- MonitorInformation/MonitorInformation.psm1
+++ MonitorInformation/MonitorInformation.psm1
@@ -1,5 +1,5 @@
 ﻿<#
-    Copyright (C) 2022 Robin Stolpe. <https://stolpe.io>
+    Copyright (C) 2023 Robin Stolpe. <https://stolpe.io>
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
